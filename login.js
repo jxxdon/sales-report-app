979ddaad00040cbf14321a0e732ed2b121215d77
jxxdon <ngawweng@gmail.com>
2025-12-18 09:36:01 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,21 +1,8 @@
-import { getAuth, signInWithEmailAndPassword } 
-  from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { signInWithEmailAndPassword } 
+  from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 
-import { initializeApp } 
-  from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { auth } from "./firebase.js";
 
-// 🔹 config Firebase kamu
-const firebaseConfig = {
-  apiKey: "API_KEY_KAMU",
-  authDomain: "PROJECT_ID.firebaseapp.com",
-  projectId: "PROJECT_ID",
-};
-
-// 🔹 init
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-// 🔹 function login (tetap namanya)
 window.login = function () {
   const email = document.getElementById("username").value;
   const password = document.getElementById("password").value;
